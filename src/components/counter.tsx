7a6e3fc101985898939b8de5d2848af62609f77b
Yogesh Kumar <yksainimbd@gmail.com>
2024-09-26 23:02:03 +0530

    useState

---
+++ src/components/counter.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div className='space-y-10'>
+      <h1 className='text-3xl font-bold underline'>
+        Simple counter: <span className='ml-8'>{count}</span>
+      </h1>
+      <div className='flex gap-2'>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
